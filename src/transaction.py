57ca3e4e4fbd6ea31f175025ad4a2b12fa35e4a0
supertren <supertren@hotmail.com>
2025-03-01 04:01:42 +0100

    a

--- src/transaction.py
+++ src/transaction.py
@@ -3,14 +3,3 @@ class Transaction:
         self.sender = sender
         self.receiver = receiver
         self.amount = amount
-
-# Example usage
-blockchain = Blockchain()
-
-# Add a transaction
-tx1 = Transaction("Alice", "Bob", 50)
-blockchain.add_block(Block(1, "", time.time(), [tx1.__dict__]))
-
-# Print the chain
-for block in blockchain.chain:
-    print(f"Block {block.index}: {block.hash}")
