0059433179af8b1024b701e4cbd93cd3399cf4fa
supertren <43068656+supertren@users.noreply.github.com>
2025-03-01 03:20:09 +0100

    Create blockchain.py

---
+++ src/blockchain.py
@@ -0,0 +1,46 @@
+import hashlib
+import time
+import json
+
+class Block:
+    def __init__(self, index, previous_hash, timestamp, transactions, nonce=0):
+        self.index = index
+        self.previous_hash = previous_hash
+        self.timestamp = timestamp
+        self.transactions = transactions
+        self.nonce = nonce
+        self.hash = self.calculate_hash()
+
+    def calculate_hash(self):
+        block_string = json.dumps({
+            "index": self.index,
+            "previous_hash": self.previous_hash,
+            "timestamp": self.timestamp,
+            "transactions": self.transactions,
+            "nonce": self.nonce
+        }, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+
+class Blockchain:
+    def __init__(self):
+        self.chain = [self.create_genesis_block()]
+        self.difficulty = 4  # Number of leading zeros required in hash
+
+    def create_genesis_block(self):
+        return Block(0, "0", time.time(), ["Genesis Block"])
+
+    def get_latest_block(self):
+        return self.chain[-1]
+
+    def add_block(self, new_block):
+        new_block.previous_hash = self.get_latest_block().hash
+        new_block.hash = new_block.calculate_hash()
+        self.mine_block(new_block)
+        self.chain.append(new_block)
+
+    def mine_block(self, block):
+        target = "0" * self.difficulty
+        while block.hash[:self.difficulty] != target:
+            block.nonce += 1
+            block.hash = block.calculate_hash()
+        print(f"Block mined! Hash: {block.hash}")
