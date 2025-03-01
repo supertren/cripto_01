584fbaac77bdfe639242c5c3a65d75d596b87fbc
supertren <supertren@hotmail.com>
2025-03-01 03:53:15 +0100

    commit all

--- src/broadcaster.py
+++ src/broadcaster.py
@@ -1,13 +1,13 @@
 from flask import Flask, jsonify
+from blockchain import Blockchain
 
 app = Flask(__name__)
+blockchain = Blockchain()
 
 @app.route('/chain', methods=['GET'])
 def get_chain():
     chain_data = [block.__dict__ for block in blockchain.chain]
     return jsonify({"chain": chain_data})
 
-if __name__ == '__main__':
-    app.run(port=5000)   # Run multiple instances on different ports to simulate a network.
-
-
+if __name__ == "__main__":
+    app.run(port=5000)
--- src/create_transaction.py
+++ src/create_transaction.py
@@ -1,2 +1,8 @@
-tx2 = Transaction("Bob", "Charlie", 20)
-blockchain.add_block(Block(2, "", time.time(), [tx2.__dict__]))
+from transaction import Transaction
+
+def create_tx(sender, receiver, amount):
+    return Transaction(sender, receiver, amount)
+
+if __name__ == "__main__":
+    tx = create_tx("Alice", "Bob", 50)
+    print(tx.__dict__)
